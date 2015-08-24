0a4c4549f16434c120e54a62b6f25cad73072f7d
Nikhil Mashettiwar <nikhil@mashettiwar.com>
2015-08-23 22:29:23 -0700

    Added group context

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import GroupMe from 'groupme'
 import Jarvis from 'jarvis-node'
 
-const jarvis = new Jarvis('jarvis.ironbay.digital', 'groupme')
+const jarvis = new Jarvis('jarvis.ironbay.digital')
 
 const ACCESS_TOKEN = "f05790b02c290133137e459f6eb8b180";
-const GROUP_ID = "15713716";
+// const GROUP_ID = "15713716";
 const USER_ID = "30100042"
 
 var API = GroupMe.Stateless;
@@ -12,8 +12,9 @@ var API = GroupMe.Stateless;
 var iStream = new GroupMe.IncomingStream(ACCESS_TOKEN, USER_ID);
 
 iStream.on('message', function (msg) {
-	const context = jarvis.getContext()
+	const context = { type: 'groupme' }
 	context.identity = msg.data.subject.sender_id
+	context.channel = msg.data.subject.group_id
 	if (context.identity === USER_ID)
 		return
 
@@ -37,15 +38,15 @@ async function reconnect() {
 reconnect()
 
 jarvis.forever('conversation.stringable', (ev) => {
-	const channel = ev.context.type == 'broadcast' ? 'UgxLnBmKV-j-EB0hlm14AaABAQ' : ev.context.channel
-	console.log('what is ev', ev)
+	console.log('what is ev message', ev.model.message);
 	let opts = {
 		message: {
 			text: '2.0: ' + ev.model.message,
 		},
 	};
 
-	API.Messages.create(GROUP_ID, opts, function(response) {
-		console.log('response: ', response);
+	API.Messages.create(ACCESS_TOKEN, ev.context.channel, opts, function(err, response) {
+		if(!err)
+			console.log('response sent: ', response);
 	})
-})
+}, 'groupme')
